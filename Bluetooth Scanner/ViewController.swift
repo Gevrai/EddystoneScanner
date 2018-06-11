eee18d4f203da1c122d4bbe36cd898a8d9a1329f
Gevrai <gevrai@gmail.com>
2018-06-10 20:17:05 -0400

    Initial Commit

---
+++ Bluetooth Scanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bluetooth Scanner
+//
+//  Created by iosdev Cdrv on 2018-06-10.
+//  Copyright © 2018 Cdrv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
